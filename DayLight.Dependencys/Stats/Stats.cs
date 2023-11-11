e603ea3460f9131db3c2e01a5db761b7b02fb9a3
Tiliboyy <tilaurel05@gmail.com>
2023-11-11 16:13:27 +0100

    Remove Newtonsoft.Json.xml from Debug bin
    Deleted the Newtonsoft.Json.xml file from Debug bin, as it was unnecessary for project build. This has been done to clean up the Debug bin and remove unused or unneeded files.

--- DayLight.Dependencys/Stats/Stats.cs
+++ DayLight.Dependencys/Stats/Stats.cs
@@ -1,45 +1,238 @@
 ﻿using DayLight.DiscordSync.Dependencys.Utils;
 using LiteDB;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace DayLight.DiscordSync.Dependencys.Stats;
-public class DatabasePlayer
+using System.ComponentModel;
+
+public interface IDatabasePlayer
 {
+    
+    public bool IsDummy { get; } 
+    public string SteamID { get; }
+    public string Nickname { get; set; }
+    public Stats Stats { get; set; }
+    public bool Profileprivate { get; set; }
+    public List<Warn> Warns { get; set; }
+    
+    public event PropertyChangedEventHandler PropertyChanged;
+    
+    
+
+}
+public class DummyDatabasePlayer : IDatabasePlayer
+{
+
+    public bool IsDummy { get; } = true;
+    public string SteamID { get; } = "Dummy";
+    public string Nickname { get; set; } = "Dummy";
+    public Stats Stats { get; set; } = null;
+    public bool Profileprivate { get; set; } = true;
+    public List<Warn> Warns { get; set; } = new List<Warn>();
+    public event PropertyChangedEventHandler PropertyChanged;
+}
+
+public class DatabasePlayer : INotifyPropertyChanged, IDatabasePlayer
+{
+    public bool IsDummy { get; } = false;
+    private string steamID;
+    private string nickname;
+    private Stats stats;
+    private bool profileprivate;
+    private List<Warn> warns;
+
     public DatabasePlayer(string steam64SteamID, string nickname)
     {
         SteamID = steam64SteamID;
         Nickname = nickname;
         Stats = new Stats();
-        Private = false;
+        Profileprivate = false;
         Warns = new List<Warn>();
     }
+
     [BsonId]
-    public string SteamID { get; private set; }
+    public string SteamID
+    {
+        get => steamID;
+        private set
+        {
+            steamID = value;
+            OnPropertyChanged(nameof(SteamID));
+        }
+    }
 
-    public string Nickname { get; set; }
+    public string Nickname
+    {
+        get => nickname;
+        set
+        {
+            nickname = value;
+            OnPropertyChanged(nameof(Nickname));
+        }
+    }
 
-    public Stats Stats { get; private set; }
-    public bool Private { get; set; }
-    
-    public List<Warn> Warns { get; set; }
+    public Stats Stats
+    {
+        get => stats;
+        set
+        {
+            stats = value;
+            OnPropertyChanged(nameof(Stats));
+        }
+    }
 
+    public bool Profileprivate
+    {
+        get => profileprivate;
+        set
+        { 
+            profileprivate = value;
+            OnPropertyChanged(nameof(Profileprivate));
+        }
+    }
 
+    public List<Warn> Warns
+    {
+        get => warns;
+        set
+        {
+            warns = value;
+            OnPropertyChanged(nameof(Warns));
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 }
-public class Stats
+public class Warns
 {
-    public double Kills { get; set; }
-    public double KilledScps { get; set; }
-    public double PinkCandyKills { get; set; }
-    public double Deaths { get; set; }
-    public double PlayedRounds { get; set; }
-    public double FastestEscapeSeconds { get; set; } 
-    
-    public float Money { get; set; }
-
-    
-    public Dictionary<ItemType, float> UsedItems { get; set; } = new();
-
-    public List<double> UnlockedAchivements { get; set; } = new();
+    private List<Warn> warns = new List<Warn>();
+    public void AddWarn()
+    {
+        
+    }
 }
 
+public class Stats : INotifyPropertyChanged
+{
+    private double kills;
+    private double killedScps;
+    private double pinkCandyKills;
+    private double deaths;
+    private double playedRounds;
+    private double fastestEscapeSeconds;
+
+    private float money;
+
+    private Dictionary<ItemType, float> usedItems;
+    private List<double> unlockedAchievements;
+
+    public Stats()
+    {
+        UsedItems = new Dictionary<ItemType, float>();
+        UnlockedAchievements = new List<double>();
+    }
+
+    public double Kills
+    {
+        get => kills;
+        set
+        {
+            kills = value;
+            OnPropertyChanged(nameof(Kills));
+        }
+    }
+
+    public double KilledScps
+    {
+        get => killedScps;
+        set
+        {
+            killedScps = value;
+            OnPropertyChanged(nameof(KilledScps));
+        }
+    }
+
+    public double PinkCandyKills
+    {
+        get => pinkCandyKills;
+        set
+        {
+            pinkCandyKills = value;
+            OnPropertyChanged(nameof(PinkCandyKills));
+        }
+    }
+
+    public double Deaths
+    {
+        get => deaths;
+        set
+        {
+            deaths = value;
+            OnPropertyChanged(nameof(Deaths));
+        }
+    }
+
+    public double PlayedRounds
+    {
+        get => playedRounds;
+        set
+        {
+            playedRounds = value;
+            OnPropertyChanged(nameof(PlayedRounds));
+        }
+    }
+
+    public double FastestEscapeSeconds
+    {
+        get => fastestEscapeSeconds;
+        set
+        {
+            fastestEscapeSeconds = value;
+            OnPropertyChanged(nameof(FastestEscapeSeconds));
+        }
+    }
+
+    public float Money
+    {
+        get => money;
+        set
+        {
+            money = value;
+            OnPropertyChanged(nameof(Money));
+        }
+    }
+
+    public Dictionary<ItemType, float> UsedItems
+    {
+        get => usedItems;
+        set
+        {
+            usedItems = value;
+            OnPropertyChanged(nameof(UsedItems));
+        }
+    }
+
+    public List<double> UnlockedAchievements
+    {
+        get => unlockedAchievements;
+        private set
+        {
+            unlockedAchievements = value;
+            OnPropertyChanged(nameof(UnlockedAchievements));
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
 
